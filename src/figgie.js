274fd13c7f59754b2b6b1e41d1af728a84c81771
justinnamilee <justin.nami.lee@gmail.com>
2023-10-06 16:19:53 -0300

    functions implemented and debugged

--- src/figgie.js
+++ src/figgie.js
@@ -6,31 +6,68 @@ import path from "path";
 export default class figgie {
   base;
   config;
-  ex;
+  express;
   server;
 
   constructor(base, config) {
     this.base = base;
     this.config = config;
-    this.ex = express();
+    this.express = express();
 
-    // ! express static
-    this.ex.use(express.static(path.join(this.base, config.publicFolder)));
-
-    //! add static route stuff here
-    this.ex.get(
+    // express routes
+    this.express.get(
       this.config.route.root,
-      (req, res) => { res.redirect(this.config.rootFile) }
+      (req, res) => { res.sendFile(path.join(this.base, this.config.folder.public, this.config.file.about)) }
     );
 
-    //! add dynamic route stuff here
-    //TODO: everything
+    this.express.get(
+      this.config.route.font,
+      (req, res) => figlet.fonts((err, font) => {
+        if (err) {
+          res.type("text");
+          res.send(err)
+        } else {
+          res.json(font)
+        }
+      })
+    );
+
+    this.express.get(
+      this.config.route.default,
+      (req, res) => {
+        if (req.params && this.config.param.text in req.params) {
+          res.type("text");
+
+          figlet(req.params[this.config.param.text], (err, output) => {
+            res.send(err ? err : this.trim(output))
+          });
+        } else {
+          res.redirect(this.config.route.root);
+        }
+      }
+    );
+
+    this.express.get(
+      this.config.route.extended,
+      (req, res) => {
+        if (req.params && this.config.param.text in req.params && this.config.param.font in req.params) {
+          res.type("text");
+
+          figlet(req.params[this.config.param.text], req.params[this.config.param.font], (err, output) => {
+            res.send(err ? err : this.trim(output))
+          });
+        } else {
+          res.redirect(this.config.route.root);
+        }
+      }
+    );
   }
 
+  // server control
   open() {
-    this.ex.use(favico(path.join(this.base, this.config.publicFolder, this.config.favico)));
+    this.express.use(favico(path.join(this.base, this.config.folder.public, this.config.favico)));
 
-    this.server = this.ex.listen(this.config.port, () => {
+    this.server = this.express.listen(this.config.port, () => {
       console.log(this.config.ui.startup + this.server.address().port);
     });
   }
@@ -42,5 +79,16 @@ export default class figgie {
       () => { console.log(this.config.ui.shutdownFail); err() },
       this.config.graceTime * 1000
     );
+  }
+
+  // clean up text
+  trim(input) {
+    let output = input;
+
+    if (this.config.trim) {
+      output = input.replace(/\s+\n/g, "\n");
+    }
+
+    return (output);
   }
 }
